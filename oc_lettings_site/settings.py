3d17827c6c899aa9bfc0958283f87f496316996e
boukaii <guillaume27121993@gmail.com>
2023-05-10 14:28:47 +0200

    p.13

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,9 @@
-import os
 import django_heroku
-# import sentry_sdk
 # import environ
+import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,11 +117,30 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-
-django_heroku.settings(locals())
-TEST_RUNNER = 'django_on_heroku.HerokuDiscoverRunner'
-
-
+#
+# django_heroku.settings(locals())
+# TEST_RUNNER = 'django_on_heroku.HerokuDiscoverRunner'
+#
+#
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+
+
+SENTRY_DSN = os.environ.get('SENTRY_DSN')
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
